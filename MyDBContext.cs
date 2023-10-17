e9b3de276a053a2f9f5529f39419399db9cb78b1
Karim Altir <mhd.altir@student.ehb.be>
2023-10-17 11:36:34 +0200

    created class to make a dummy data and save it in database

---
+++ MyDBContext.cs
@@ -0,0 +1,45 @@
+﻿using Administratie.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administratie
+{
+    public class MyDBContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public void InitiateDatabase()
+        {
+            ProductCategory pc = new ProductCategory() { Name = "Technology" };
+            if (!ProductCategories.Any())
+            {
+                ProductCategories.Add(pc);
+            }
+            else
+            {
+                pc = ProductCategories.First();
+            }
+
+            if (!Products.Any())
+            {
+                Product product = new Product() { Name = "Sony", Description = "Electricity", Category = pc };
+                Products.Add(product);
+            }
+            SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost\\SQLEXPRESS;"
+                                        + "Database = Administatie;"
+                                        + "Trusted_Connection = True;"
+                                        + "MultipleActiveResultSets = true;"
+                                        + "TrustServerCertificate = true");
+        }
+    }
+}
